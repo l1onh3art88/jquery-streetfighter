06a3acb7a5350c3e2103bf2b55edf3e2f4e715e6
l1onh3art88 <spenccheng@gmail.com>
2015-01-06 16:50:42 -0600

    finished streetfighter 1.0

--- js/app.js
+++ js/app.js
@@ -22,8 +22,22 @@ $(document).ready(function() {
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
-  });
-});
+  })
+  $(document).finish().keydown(function(e){
+    if(e.keyCode == 88) {
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }
+    
+  })
+  .keyup(function(e){
+    if (e.keyCode == 88){
+      $('.ryu-cool').hide();
+      $('.ryu-ready').show();
+    }
+  })
+})
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
